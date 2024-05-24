459c04eb11fcf4d9474c0781a83aa094d1212e40
maintn24 <maintn124@gmail.com>
2024-05-24 10:39:15 +0700

    feat: create PUT (update an object) endpoint

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -27,11 +27,29 @@ router.get('/:id', async (req, res) => {
 })
 
 
-//create (add) a new student
+//create (add) new students
 router.post('/', async (req, res) => {
     try {
         const student = await StudentInfo.create(req.body);
         res.status(200).json(student);
+    } catch (error) {
+        // @ts-ignore
+        res.status(500).json({message: error.message});
+    }
+});
+
+//update
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const student = await StudentInfo.findByIdAndUpdate(id, req.body);
+
+        if (!student) {
+            return res.status(404).json({message: "Student not found"});
+        }
+
+        const updatedStudent = await StudentInfo.findById(id);
+        res.status(200).json(updatedStudent);
     } catch (error) {
         // @ts-ignore
         res.status(500).json({message: error.message});
